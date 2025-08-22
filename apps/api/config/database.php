350c64e7f6f13ab6ab95d62b8d5afa2b7038b19e
Nezzar KEFIF <36443340+nezz0746@users.noreply.github.com>
2025-08-22 16:24:39 +0200

    fix: update env variables in laravel services

--- apps/api/config/database.php
+++ apps/api/config/database.php
@@ -45,11 +45,11 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DB_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'laravel'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => env('MYSQLHOST', '127.0.0.1'),
+            'port' => env('MYSQLPORT', '3306'),
+            'database' => env('MYSQLDATABASE', 'laravel'),
+            'username' => env('MYSQLUSER', 'root'),
+            'password' => env('MYSQLPASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => env('DB_CHARSET', 'utf8mb4'),
             'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
@@ -147,7 +147,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug((string) env('APP_NAME', 'laravel')).'-database-'),
+            'prefix' => env('REDIS_PREFIX', Str::slug((string) env('APP_NAME', 'laravel')) . '-database-'),
             'persistent' => env('REDIS_PERSISTENT', false),
         ],
 
