8745df8493060703a807a430afcebbc83afff7d8
Nezzar KEFIF <36443340+nezz0746@users.noreply.github.com>
2025-08-22 17:37:25 +0200

    eslint conf

--- apps/web/eslint.config.mjs
+++ apps/web/eslint.config.mjs
@@ -20,6 +20,14 @@ const eslintConfig = [
       "next-env.d.ts",
     ],
   },
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": "off",
+      "react/no-unescaped-entities": "off",
+      "@next/next/no-html-link-for-pages": "off",
+      "@typescript-eslint/ban-ts-comment": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
